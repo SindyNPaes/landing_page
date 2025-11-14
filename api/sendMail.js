bca155c13e7939c470b6a22b66b5ad9e25ce14a5
GustavoPucienik <gomespucienik@gmail.com>
2025-11-14 00:09:51 -0300

    teste

--- api/sendMail.js
+++ api/sendMail.js
@@ -1,10 +1,34 @@
+import nodemailer from "nodemailer";
 
-export default function handler(req, res) {
-  res.status(200).json({
-    "SMTP_HOST": String(process.env.SMTP_HOST),
-    "SMTP_PORT": String(process.env.SMTP_PORT),
-    "SMTP_USER": String(process.env.SMTP_USER),
-    "SMTP_PASS": String(process.env.SMTP_PASS),
-  });
-}
+export const sendEmail = async (req, res) => {
+  const { to, subject, message } = req.body;
+  console.log(to, subject, message)
 
+  if (!to || !subject || !message) {
+    return res.status(400).json({ error: "Campos obrigatórios ausentes." });
+  }
+
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: true, // use true se for porta 465
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: `"Sindy Paes Advogada" <${process.env.SMTP_USER}>`,
+      to,
+      subject,
+      html: `<p>${message}</p>`,
+    });
+
+    return res.json({ success: true, message: "E-mail enviado com sucesso!" },to,subject,message);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Erro ao enviar e-mail." },to,subject,message);
+  }
+};
