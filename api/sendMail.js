580c537baebaabd1b885913ce32536d8243a6dad
GustavoPucienik <gomespucienik@gmail.com>
2025-11-14 00:03:21 -0300

    teste

--- api/sendMail.js
+++ api/sendMail.js
@@ -1,11 +1,10 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    SMTP_HOST: String(process.env.SMTP_HOST),
-    SMTP_PORT: String(process.env.SMTP_PORT),
-    SMTP_USER: String(process.env.SMTP_USER),
-    SMTP_PASS: String(process.env.SMTP_PASS),
-    SMTP_TO: String(process.env.SMTP_TO),
+    "SMTP_HOST": String(process.env.SMTP_HOST),
+    "SMTP_PORT": String(process.env.SMTP_PORT),
+    "SMTP_USER": String(process.env.SMTP_USER),
+    "SMTP_PASS": String(process.env.SMTP_PASS),
   });
 }
 
