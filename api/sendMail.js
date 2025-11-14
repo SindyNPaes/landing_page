7c2c83d92ed3a4e5d810db18d4734f5208aec4ff
GustavoPucienik <gomespucienik@gmail.com>
2025-11-13 23:56:56 -0300

    teste

--- api/sendMail.js
+++ api/sendMail.js
@@ -1,42 +1,11 @@
-import nodemailer from "nodemailer";
 
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Método não permitido" });
-  }
-
-  const { name, email, message } = req.body;
-
-  if (!name || !email || !message) {
-    return res.status(400).json({ error: "Campos obrigatórios ausentes." });
-  }
-
-  try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      secure: false,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
-
-    await transporter.sendMail({
-      from: `"Site - Formulário de Contato" <${process.env.SMTP_USER}>`,
-      to: process.env.SMTP_TO, // Email que vai receber no escritório
-      subject: `Nova mensagem de ${name}`,
-      html: `
-        <h2>Nova mensagem do site</h2>
-        <p><strong>Nome:</strong> ${name}</p>
-        <p><strong>Email:</strong> ${email}</p>
-        <p><strong>Mensagem:</strong><br>${message}</p>
-      `,
-    });
-
-    return res.json({ success: true, message: "E-mail enviado com sucesso!" });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Erro ao enviar e-mail." });
-  }
+export default function handler(req, res) {
+  res.status(200).json({
+    SMTP_HOST: String(process.env.SMTP_HOST),
+    SMTP_PORT: String(process.env.SMTP_PORT),
+    SMTP_USER: String(process.env.SMTP_USER),
+    SMTP_PASS: String(process.env.SMTP_PASS),
+    SMTP_TO: String(process.env.SMTP_TO),
+  });
 }
+
--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -14,7 +14,7 @@ export default function ContactForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     // Aqui você integra com EmailJS ou seu backend
-    console.log("Enviando dados v2:", formData);
+    console.log("Enviando dados v3:", formData);
     console.log("SMTP_HOST:", process.env.SMTP_HOST);
     console.log("SMTP_PORT:", process.env.SMTP_PORT);
     console.log("SMTP_USER:", process.env.SMTP_USER);
@@ -32,6 +32,7 @@ export default function ContactForm() {
           message: formData.message,
         }),
       });
+      console.log(response.data)
 
       const data = await response.json();
 
